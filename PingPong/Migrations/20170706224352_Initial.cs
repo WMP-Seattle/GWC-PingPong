f769094703092c9dadb3853e2b363d642bd12daa
Gage Black <0gageblack0@gmail.com>
2017-07-06 15:45:11 -0700

    Reinitialized the databse. Added the Games table.

---
+++ PingPong/Migrations/20170706224352_Initial.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PingPong.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Office = table.Column<string>(nullable: true),
+                    numberLosses = table.Column<int>(nullable: false),
+                    numberWins = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PlayerOneId = table.Column<int>(nullable: false),
+                    PlayerTwoId = table.Column<int>(nullable: false),
+                    Winner = table.Column<int>(nullable: false),
+                    complete = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Games_Players_PlayerOneId",
+                        column: x => x.PlayerOneId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Games_Players_PlayerTwoId",
+                        column: x => x.PlayerTwoId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_PlayerOneId",
+                table: "Games",
+                column: "PlayerOneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_PlayerTwoId",
+                table: "Games",
+                column: "PlayerTwoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
--- PingPong/Models/Players.cs
+++ PingPong/Models/Players.cs
@@ -2,16 +2,16 @@ using System;
 using System.ComponentModel.DataAnnotations;
 
 //Players table to hold First Name, Last Name, and Number of Wins
-namespace PlayersAPI
+namespace PingPong.Entities
 {
-    public class PlayerData
+    public class Player
     {
         [Key]
         public int Id { get; set; }
         [Required]
-        [MaxLength(20)]
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public string Office { get; set; }
         public int numberWins {get; set; }
         public int numberLosses {get; set;}
     }
